05d0d10910cee6d7ce5338e607787480c054e120
tlummm <sid.gomesjr@gmail.com>
2026-09-20 12:54:30 -0300

    luta muito básica

--- Jogo/Program.cs
+++ Jogo/Program.cs
@@ -1,1 +1,60 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Variáveis
+var suavida = 100;
+var seudano = 25;
+
+var esqueletoVida = 100;
+var esqueletoDano = 50;
+
+// Luta
+Console.Clear();
+
+while (suavida > 0 & esqueletoVida > 0)
+{    
+    falar("Esqueleto está na sua frente");
+    falar("Você ataca (1) ou foge? (2)");
+    var escolha = ouvir();
+
+    if (escolha == "1")
+    {
+        falar("Você atacou o esqueleto");
+        esqueletoVida -= seudano;
+    }
+
+    else if (escolha == "2")
+    {
+        break;
+    }
+
+    else
+    {
+        falar("Comando inválido");
+        suavida -= esqueletoDano;
+    }
+}
+
+// Resolução
+if (esqueletoVida <= 0)
+{
+    falar("Você derrotou o esqueleto");
+}
+
+else if (suavida <= 0)
+{
+    falar("Você foi derrotado");
+}
+
+else
+{
+    falar ("Você fugiu");
+}
+
+// Auxílio
+static void falar (string parametro)
+{
+    Console.WriteLine(parametro);
+}
+
+static string? ouvir ()
+{
+    return Console.ReadLine();
+}
--- Jogo/obj/Debug/net10.0/Jogo.AssemblyInfo.cs
+++ Jogo/obj/Debug/net10.0/Jogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd681ec43bcbf498599bffb93cd336cc859ba1e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+917ab192f7a030faa58dd15ec9a3ad0453152641")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
